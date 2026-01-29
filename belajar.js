fdff444607fc794f8105c37af18532f4bfd62719
Kurooo21 <daffaabiyan311@gmail.com>
2026-01-29 07:33:35 +0700

    first commit js

---
+++ belajar.js
@@ -0,0 +1,33 @@
+// alert("Halo bg");
+// console
+// console.log("Hello World");
+// prompt
+// prompt("Masukkan nama Anda");
+
+
+//variable
+// console.log("=== Variable ===");
+// let nama = "Andi";
+// var nama = "Andi";
+// const nama = "Andi";
+
+//PERkONDISIAN
+// if (true) {
+//     console.log("Benar");
+// } else {
+//     console.log("Salah");
+// }
+
+// switch (new Date().getDay()) {
+//     case 0:
+//         console.log("Minggu");
+//         break;
+//     case 1:
+//         console.log("Senin");
+//         break;
+//     case 2:
+//         console.log("Selasa");
+//         break;
+// }
+
+ 
