6982a7e0f47c5bb32c86ba408404527a622c87d9
Kurooo21 <daffaabiyan311@gmail.com>
2026-02-01 20:21:26 +0700

    materi baru

--- belajar.js
+++ belajar.js
@@ -30,4 +30,34 @@
 //         break;
 // }
 
- 
+// manipulasi DOM
+//  document.title = "Belajar JavaScript";
+//  const body = document.body;
+
+//  const h1 = document.createElement('h1');
+//     h1.textContent = "Halo, ini judul utama!";
+
+// const p = document.createElement('p');
+//     p.innerHTML = "<marquee> Ini adalah paragraf pertama di halaman ini </marquee>";
+
+// const b = document.createElement('b');
+//     b.textContent = "Teks ini dicetak tebal.";
+
+// //<marquee> </maequee> >membuat teks berjalan hanaya bisa digunaklan di ineerHTML
+// body.append(h1);
+// body.append(p);
+// body.append(b);
+
+// //================\\
+// document.title = "Belajar JavaScript";
+// const btn1 = document.getElementById('btn1');
+// const btn2 = document.getElementById('btn2');
+// // styling button 1
+// btn1.style.border ="none";
+// btn1.style.padding ="10px 20px";
+// btn1.style.backgroundColor ="gray";
+// btn1.style.color ="white";
+// btn1.style.cursor ="pointer";
+// btn1.style.marginRight ="10px";
+// btn1.style.borderRadius ="5px";
+
